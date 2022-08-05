1177ea9fd8d0791fd243723fb804d6db7ca76534
DESKTOP-I94OD7A\user <kyu@gmail.com>
2022-08-05 18:44:23 +0900

    220805 커밋

--- src/main/java/com/noticeboard/user/userController.java
+++ src/main/java/com/noticeboard/user/userController.java
@@ -1,17 +1,29 @@
 package com.noticeboard.user;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/user")
 public class userController {
 
+	// http://localhost/user/sign_in_view
+	@RequestMapping("/sign_in_view")
+	public String signInView(Model model) {
+			
+		model.addAttribute("viewName","sign_in");
+		model.addAttribute("folder","user");
+		return "/template/layout";
+	}
+	
+	// http://localhost/user/sign_up_view
 	@RequestMapping("/sign_up_view")
-	public String signUpView() {
+	public String signUpView(Model model) {
 		
-		
-		return "/user/sign_up";
+		model.addAttribute("viewName","sign_up");
+		model.addAttribute("folder","user");
+		return "/template/layout";
 	}
 	
 }
