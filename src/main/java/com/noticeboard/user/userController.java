984597dc1898bcd31ffd12382256314a3a18c993
DESKTOP-I94OD7A\user <kyu@gmail.com>
2022-08-05 18:11:26 +0900

    초기환경 세팅

--- src/main/java/com/noticeboard/user/userController.java
+++ src/main/java/com/noticeboard/user/userController.java
@@ -1,5 +1,17 @@
 package com.noticeboard.user;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
 public class userController {
 
+	@RequestMapping("/sign_up_view")
+	public String signUpView() {
+		
+		
+		return "/user/sign_up";
+	}
+	
 }
